8f28f798736d700b91cdee682d71ccc725e9178d
Mathias Guijarro <mathias.guijarro@psi.ch>
2024-05-23 14:13:01 +0200

    feat: new cmdline argument to start redis servers

--- bec_server/bec_server/bec_server_utils/subprocess_launch.py
+++ bec_server/bec_server/bec_server_utils/subprocess_launch.py
@@ -1,20 +1,51 @@
+import functools
 import os
+import shutil
 import subprocess
 
+TERMINALS = (
+    "x-terminal-emulator",
+    "mate-terminal",
+    "gnome-terminal",
+    "terminator",
+    "xfce4-terminal",
+    "urxvt",
+    "rxvt",
+    "termit",
+    "Eterm",
+    "xterm",
+    "konsole",
+)
 
-def subprocess_start(bec_path: str, config_path: str, services: dict):
+
+@functools.cache
+def detect_terminal():
+    all_terms = [os.environ.get("TERMINAL", "")] + list(TERMINALS)
+    for term in all_terms:
+        if shutil.which(term):
+            return term
+    raise RuntimeError("Could not detect any suitable terminal to launch processes")
+
+
+def subprocess_start(bec_path: str, services: dict):
     processes = []
+
     for ii, service_info in enumerate(services.items()):
         service, service_config = service_info
-        service_path = service_config["path"].substitute(base_path=bec_path)
+
+        if os.environ.get("CONDA_DEFAULT_ENV"):
+            cmd = f"{os.environ['CONDA_EXE']} run -n {os.environ['CONDA_DEFAULT_ENV']} --no-capture-output {service_config.command}"
+        else:
+            cmd = service_config.command
+
+        service_path = service_config.path.substitute(base_path=bec_path)
         # service_config adds a subdirectory to each path, here we do not want the subdirectory
         cwd = os.path.abspath(os.path.join(service_path, ".."))
-        if config_path:
-            processes.append(
-                subprocess.Popen((service_config["command"], "--config", config_path), cwd=cwd)
-            )
-        else:
-            processes.append(subprocess.Popen((service_config["command"],), cwd=cwd))
+        try:
+            processes.append(subprocess.Popen([detect_terminal(), "-H", "-e", cmd], cwd=cwd))
+        except RuntimeError:
+            # no terminal: just execute servers in background
+            processes.append(subprocess.Popen(cmd.split(), cwd=cwd))
     return processes
 
 
