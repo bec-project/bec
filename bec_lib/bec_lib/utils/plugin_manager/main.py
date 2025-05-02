a899dcdc152167a079b1e1c82eeb6e3c10227244
David Perl <david.perl@psi.ch>
2025-05-02 16:57:14 +0200

    feat(plugins): add cli app to manage plugins

---
+++ bec_lib/bec_lib/utils/plugin_manager/main.py
@@ -0,0 +1,20 @@
+import typer
+
+from bec_lib.logger import bec_logger
+from bec_lib.utils.plugin_manager import create
+from bec_lib.utils.plugin_manager.edit_ui import open_and_watch_ui_editor
+
+logger = bec_logger.logger
+_app = typer.Typer()
+_app.add_typer(create._app)
+
+
+@_app.command()
+def edit_ui(widget_name: str):
+    """Edit the ui for a given widget plugin"""
+    open_and_watch_ui_editor(widget_name)
+
+
+def main():
+    """Initial entrypoint for bec-plugin-manager"""
+    _app()
