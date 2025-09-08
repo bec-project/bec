fea8c86c96fd464d4449b340740b3f256410def9
wakonig_k <klaus.wakonig@psi.ch>
2025-09-08 15:39:51 +0200

    fix(atlas): scope ingestion messages by stream keys

--- bec_server/tests/tests_scihub/test_atlas_metadata_handler.py
+++ bec_server/tests/tests_scihub/test_atlas_metadata_handler.py
@@ -22,7 +22,7 @@ def test_atlas_metadata_handler(atlas_connector):
         atlas_connector.metadata_handler._handle_scan_status(
             msg_obj, parent=atlas_connector.metadata_handler
         )
-        mock_ingest_data.assert_called_once_with({"data": msg})
+        mock_ingest_data.assert_called_once_with({"scan_status": msg})
 
     with mock.patch.object(
         atlas_connector.metadata_handler, "update_scan_status", side_effect=ValueError
