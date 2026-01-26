9a78fed15eb4db0f75f12d821d06e147845cc184
David Perl <david.perl@psi.ch>
2026-01-26 15:55:51 +0100

    feat: add subprocess worker

---
+++ bec_server/bec_server/procedures/multiprocess_worker.py
@@ -0,0 +1,52 @@
+import os
+import subprocess
+import sys
+
+from bec_lib.logger import bec_logger
+from bec_server.procedures.oop_worker_base import OutOfProcessWorkerBase, main
+
+logger = bec_logger.logger
+
+
+class SubProcessWorker(OutOfProcessWorkerBase):
+    """A worker which runs scripts in a container with a full BEC environment,
+    mounted from the filesystem, and only access to Redis"""
+
+    def _setup_execution_environment(self):
+        env: dict[str, str] = self._worker_environment()  # type: ignore
+        env.update({"redis_server": "localhost:6379"})
+        self._process = subprocess.Popen(
+            [sys.executable, __file__],
+            stdout=subprocess.PIPE,
+            text=True,
+            bufsize=1000,
+            env=os.environ | env,
+        )
+        logger.success("Subprocess Procedure Worker started.")
+
+    def _ending_or_ended(self):
+        return self._process.poll() is not None
+
+    def _kill_process(self):
+        if not self._ending_or_ended():
+            self._process.kill()
+
+    def abort_execution(self, execution_id: str):
+        """Abort the execution with the given id. Has no effect if the given ID is not the current job"""
+        if execution_id == self._current_execution_id:
+            self._process.kill()
+            self._helper.remove_from_active.by_exec_id(execution_id)
+            logger.info(
+                f"Aborting execution {execution_id}, restarting worker for queue: {self._queue}"
+            )
+            self._setup_execution_environment()
+
+    def logs(self):
+        """"""
+        if self._process.stdout is None:
+            return []
+        return list(self._process.stdout)
+
+
+if __name__ == "__main__":
+    main()
