5db8246798680373a3f6c911242117c64e143d6a
wakonig_k <klaus.wakonig@psi.ch>
2026-03-03 13:16:35 +0100

    fix(redis connector): retry on BusyLoadingError

--- bec_lib/bec_lib/redis_connector.py
+++ bec_lib/bec_lib/redis_connector.py
@@ -349,6 +349,7 @@ class RedisConnector:
             supported_errors=(
                 redis.exceptions.TimeoutError,
                 redis.exceptions.ConnectionError,
+                redis.exceptions.BusyLoadingError,
                 ConnectionRefusedError,
                 OSError,
                 socket.timeout,
