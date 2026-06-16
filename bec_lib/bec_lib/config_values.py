9b2f77237b95fc34bf4ae8fa10322e70c73315cf
perl_d <david.perl@psi.ch>
2026-06-16 15:40:11 +0200

    feat: add option to wait for set in redis to complete, use by default

--- bec_lib/bec_lib/config_values.py
+++ bec_lib/bec_lib/config_values.py
@@ -1,3 +1,4 @@
+from threading import Event
 from typing import Any, Callable, Generic, TypeVar
 from weakref import ReferenceType
 
@@ -12,17 +13,12 @@ logger = bec_logger.logger
 ValueT = TypeVar("ValueT")
 
 
-class UninitializedSentinel:
-    def __eq__(self, value: object, /) -> bool:
-        return False
-
-
-_UNINITIALIZED = UninitializedSentinel()
-
-
 class RedisConfigValue(property, Generic[ValueT]):
     def __init__(
-        self, connector: RedisConnector, endpoint: EndpointInfo[type[ManagedConfigMessage[ValueT]]]
+        self,
+        connector: RedisConnector,
+        endpoint: EndpointInfo[type[ManagedConfigMessage[ValueT]]],
+        wait_for_writes: bool = True,
     ) -> None:
         """A config value bound to a value in Redis, which uses the ManagedConfigMessage and an associated endpoint,
         and which can be subscribed to."""
@@ -38,6 +34,9 @@ class RedisConfigValue(property, Generic[ValueT]):
         self._cbs: set[ReferenceType[Callable[[ValueT]]] | BoundMethodWeakref] = set()
         self._config = self._fetch()
         self._connector.register(self._ep, cb=self._update_cb)
+        self._writing_wait_event = Event()
+        self._writing_wait_event.set()
+        self._wait_for_writes = wait_for_writes
 
     def __del__(self):
         if hasattr(self, "_connector"):
@@ -57,19 +56,29 @@ class RedisConfigValue(property, Generic[ValueT]):
             return config
         return existing[-1]["config"]
 
-    def _write(self, updated: ManagedConfigMessage[ValueT]):
+    def _write(self, updated: ManagedConfigMessage[ValueT], wait: bool = True):
+        if wait:
+            self._writing_wait_event.clear()
         self._connector.xadd(self._ep, {"config": updated}, max_size=1)
+        self._writing_wait_event.wait(timeout=2)
+        if not self._writing_wait_event.is_set():
+            logger.error(
+                f"Timed out waiting for config variable {self._ep.endpoint} to return from Redis"
+            )
 
     def _update_cb(self, msg_dict: dict):
-        self._config = msg_dict["config"]
-        for cb_ref in list(self._cbs):
-            if cb := cb_ref():
-                try:
-                    cb(self._config.value)
-                except Exception as e:
-                    logger.error(f"Exception in managed config value callback {cb}: {e}")
-            else:
-                self._cbs.discard(cb_ref)
+        try:
+            self._config = msg_dict["config"]
+            for cb_ref in list(self._cbs):
+                if cb := cb_ref():
+                    try:
+                        cb(self._config.value)
+                    except Exception as e:
+                        logger.error(f"Exception in managed config value callback {cb}: {e}")
+                else:
+                    self._cbs.discard(cb_ref)
+        finally:
+            self._writing_wait_event.set()
 
     @property
     def value(self) -> ValueT:
@@ -77,7 +86,7 @@ class RedisConfigValue(property, Generic[ValueT]):
 
     @value.setter
     def value(self, value: ValueT):
-        self._write(self._ep.message_type(value=value))
+        self._write(self._ep.message_type(value=value), self._wait_for_writes)
 
     def subscribe(self, cb: Callable[[ValueT], Any]):
         self._cbs.add(safe_ref(cb))
