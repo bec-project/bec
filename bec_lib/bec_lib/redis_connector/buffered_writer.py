ddeb0c4a0338e49864857cc0fae18568c65db494
David Perl <dav_perl@pm.me@>
2026-06-19 11:38:48 +0200

    wip

---
+++ bec_lib/bec_lib/redis_connector/buffered_writer.py
@@ -0,0 +1,35 @@
+from collections import defaultdict, deque
+from dataclasses import dataclass
+from functools import partial
+from time import monotonic, monotonic_ns
+from typing import Callable
+
+from bec_lib.messages import BECMessage
+
+MessageFactory = Callable[[], BECMessage]
+WriteCommand = Callable[[str, BECMessage], None]
+BufferEntry = tuple[str, WriteCommand]
+
+
+@dataclass
+class WriteBuffer:
+    message_factories: deque[MessageFactory]
+    last_called_time: float
+
+
+class BufferedWriter:
+    def __init__(self):
+        self._write_buffers: dict[BufferEntry, WriteBuffer] = defaultdict(
+            lambda: WriteBuffer(deque(), 0)
+        )
+        self._cooldown_ns = 100_000_000  # 0.1 s
+
+    def write(self, endpoint: str, command: WriteCommand, message_factory: MessageFactory):
+        entry = self._write_buffers[(endpoint, command)]
+        try:
+            if monotonic_ns() - entry.last_called_time > self._cooldown_ns:
+                command(endpoint, message_factory())
+                return
+            entry.message_factories.append(message_factory)
+        finally:
+            entry.last_called_time = monotonic_ns()
