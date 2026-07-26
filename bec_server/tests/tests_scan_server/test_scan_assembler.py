4e7989f307c8f17b1b131b7ce6b447a713c8f915
wakonig_k <klaus.wakonig@psi.ch>
2026-07-26 18:10:18 +0200

    w

--- bec_server/tests/tests_scan_server/test_scan_assembler.py
+++ bec_server/tests/tests_scan_server/test_scan_assembler.py
@@ -9,9 +9,7 @@ from bec_lib.device import DeviceBase
 from bec_lib.scan_args import ScanArgument
 from bec_lib.tests.fixtures import dm_with_devices
 from bec_server.scan_server.scan_assembler import ScanAssembler
-from bec_server.scan_server.scans import ScanArgType
 from bec_server.scan_server.scans.acquire import Acquire
-from bec_server.scan_server.scans.legacy_scans import FermatSpiralScan, LineScan, RequestBase
 from bec_server.scan_server.scans.scan_argument_modifier import scan_signature_with_modifiers
 from bec_server.scan_server.tests.utils import NoopScan
 
@@ -21,29 +19,9 @@ def scan_assembler():
     return ScanAssembler(parent=mock.MagicMock())
 
 
-class CustomScan(RequestBase):
-    scan_name = "custom_scan"
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-
-    def run(self):
-        pass
-
-
-class CustomScan2(RequestBase):
-    scan_name = "custom_scan2"
-
-    def __init__(self, arg1, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-
-    def run(self):
-        pass
-
-
 class CustomDirectScan(NoopScan):
     scan_name = "custom_direct_scan"
-    arg_input = {"device": ScanArgType.DEVICE, "target": ScanArgType.FLOAT}
+    arg_input = {"device": DeviceBase, "target": float}
     arg_bundle_size = {"bundle": len(arg_input), "min": 1, "max": None}
     is_scan = False
 
@@ -78,7 +56,7 @@ class CustomBoundedDirectScan(NoopScan):
 class CustomBundledBoundedDirectScan(NoopScan):
     scan_name = "custom_bundled_bounded_direct_scan"
     arg_input = {
-        "device": ScanArgType.DEVICE,
+        "device": DeviceBase,
         "target": Annotated[int, ScanArgument(display_name="Target", ge=1)],
     }
     arg_bundle_size = {"bundle": len(arg_input), "min": 1, "max": None}
@@ -97,7 +75,7 @@ class CustomBundledBoundedDirectScan(NoopScan):
 
 class CustomBundledScanWithDeviceKwarg(NoopScan):
     scan_name = "custom_bundled_scan_with_device_kwarg"
-    arg_input = {"device": ScanArgType.DEVICE, "target": ScanArgType.FLOAT}
+    arg_input = {"device": DeviceBase, "target": float}
     arg_bundle_size = {"bundle": len(arg_input), "min": 1, "max": None}
     is_scan = False
 
@@ -162,140 +140,6 @@ class AdditionalParamsModifier:
             ]
             defaults["integ_time"] = 0.5
         return arguments, defaults
-
-
-@pytest.mark.parametrize(
-    "msg, request_inputs_expected",
-    [
-        (
-            # Fermat scan with args and kwargs, matching the FermatSpiralScan signature
-            messages.ScanQueueMessage(
-                scan_type="fermat_scan",
-                parameter={"args": {"samx": (-5, 5), "samy": (-5, 5)}, "kwargs": {"steps": 3}},
-                queue="primary",
-            ),
-            {
-                "arg_bundle": [],
-                "inputs": {
-                    "motor1": "samx",
-                    "start_motor1": -5,
-                    "stop_motor1": 5,
-                    "motor2": "samy",
-                    "start_motor2": -5,
-                    "stop_motor2": 5,
-                },
-                "kwargs": {"steps": 3},
-            },
-        ),
-        (
-            # Fermat scan with no args; everything is in kwargs
-            messages.ScanQueueMessage(
-                scan_type="fermat_scan",
-                parameter={
-                    "args": [],
-                    "kwargs": {
-                        "motor1": "samx",
-                        "start_motor1": -5,
-                        "stop_motor1": 5,
-                        "motor2": "samy",
-                        "start_motor2": -5,
-                        "stop_motor2": 5,
-                        "steps": 3,
-                    },
-                },
-                queue="primary",
-            ),
-            {
-                "arg_bundle": [],
-                "inputs": {
-                    "motor1": "samx",
-                    "start_motor1": -5,
-                    "stop_motor1": 5,
-                    "motor2": "samy",
-                    "start_motor2": -5,
-                    "stop_motor2": 5,
-                },
-                "kwargs": {"steps": 3},
-            },
-        ),
-        (
-            # Fermat scan with mixed args and kwargs
-            messages.ScanQueueMessage(
-                scan_type="fermat_scan",
-                parameter={
-                    "args": ["samx"],
-                    "kwargs": {
-                        "start_motor1": -5,
-                        "stop_motor1": 5,
-                        "motor2": "samy",
-                        "start_motor2": -5,
-                        "stop_motor2": 5,
-                        "steps": 3,
-                    },
-                },
-                queue="primary",
-            ),
-            {
-                "arg_bundle": [],
-                "inputs": {
-                    "motor1": "samx",
-                    "start_motor1": -5,
-                    "stop_motor1": 5,
-                    "motor2": "samy",
-                    "start_motor2": -5,
-                    "stop_motor2": 5,
-                },
-                "kwargs": {"steps": 3},
-            },
-        ),
-        (
-            # Line scan with arg bundle
-            messages.ScanQueueMessage(
-                scan_type="line_scan",
-                parameter={"args": {"samx": (-5, 5), "samy": (-5, 5)}, "kwargs": {"steps": 3}},
-                queue="primary",
-            ),
-            {"arg_bundle": ["samx", -5, 5, "samy", -5, 5], "inputs": {}, "kwargs": {"steps": 3}},
-        ),
-        (
-            # Custom scan with args
-            messages.ScanQueueMessage(
-                scan_type="custom_scan",
-                parameter={"args": ["samx", -5, 5], "kwargs": {}},
-                queue="primary",
-            ),
-            {"arg_bundle": [], "inputs": {"args": ["samx", -5, 5]}, "kwargs": {}},
-        ),
-        (
-            # Custom scan with args
-            messages.ScanQueueMessage(
-                scan_type="custom_scan2",
-                parameter={"args": [True, "samx", -5, 5], "kwargs": {}},
-                queue="primary",
-            ),
-            {"arg_bundle": [], "inputs": {"arg1": True, "args": ["samx", -5, 5]}, "kwargs": {}},
-        ),
-    ],
-)
-def test_scan_assembler_request_inputs(msg, request_inputs_expected, scan_assembler):
-
-    class MockScanManager:
-        available_scans = {
-            "fermat_scan": {"class": "FermatSpiralScan"},
-            "line_scan": {"class": "LineScan"},
-            "custom_scan": {"class": "CustomScan"},
-            "custom_scan2": {"class": "CustomScan2"},
-        }
-        scan_dict = {
-            "fermat_scan": FermatSpiralScan,
-            "line_scan": LineScan,
-            "custom_scan": CustomScan,
-            "custom_scan2": CustomScan2,
-        }
-
-    with mock.patch.object(scan_assembler, "scan_manager", MockScanManager()):
-        request = scan_assembler.assemble_device_instructions(msg, "scan_id")
-        assert request.request_inputs == request_inputs_expected
 
 
 def test_scan_assembler_assemble_direct_scan_resolves_device_args(dm_with_devices):
