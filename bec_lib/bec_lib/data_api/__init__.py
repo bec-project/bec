e1efbe645472ea28556f290f0b4a2f2684996b9f
wyzula-jan <jan.wyzula@psi.ch>
2026-08-07 12:44:52 +0200

    feat(data_api): rewrite facade and live plugin on ordinal alignment

--- bec_lib/bec_lib/data_api/__init__.py
+++ bec_lib/bec_lib/data_api/__init__.py
@@ -1,1 +1,5 @@
-from .data_api import DataAPI, DataSubscription
+from .alignment import CorrelationGroupError
+from .data_api import DataAPI, Subscription
+from .live_plugin import LiveDataPlugin
+from .models import SourceData, SourceKey, SubscriptionUpdate
+from .plugin_base import DataSourcePlugin, SourceRequest, SourceSpec
